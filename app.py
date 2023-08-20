62a683580abaadc84b2811da85187d686ebc2876
Darren Ngatimin <dngatimin95@gmail.com>
2023-08-20 17:45:23 +0800

    Frozen flask to allow for static pages

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/hi')
 def hi():
@@ -35,4 +37,4 @@ def index():
     return render_template('modern.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    freezer.freeze()
