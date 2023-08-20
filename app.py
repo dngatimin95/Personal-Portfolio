29a335ce9159672820b05684404a5e56fc7b89db
Darren Ngatimin <dngatimin95@gmail.com>
2023-08-20 18:47:14 +0800

    Remove static pages

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
-from flask_frozen import Freezer
 
 app = Flask(__name__)
-freezer = Freezer(app)
 
 @app.route('/hi')
 def hi():
@@ -37,4 +35,4 @@ def index():
     return render_template('modern.html')
 
 if __name__ == "__main__":
-    freezer.freeze()
+    app.run(debug=True)
